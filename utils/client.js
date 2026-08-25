698f9a962c7fa2e4ecfac8c842f804cd89052132
Bijoy Lohar <loharbijoy2005@gmail.com>
2026-08-25 10:29:16 +0530

    Update utils/client.js and lock discord.js v14 in package-lock.json

--- utils/client.js
+++ utils/client.js
@@ -1,32 +1,19 @@
-const { Client, GatewayIntentBits, Intents, Partials } = require('discord.js');
-
-// Support both discord.js v14 (GatewayIntentBits) and v13 fallback
-const intents = GatewayIntentBits
-  ? [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-      GatewayIntentBits.GuildMembers,
-    ]
-  : [
-      Intents?.FLAGS?.GUILDS || 'GUILDS',
-      Intents?.FLAGS?.GUILD_MESSAGES || 'GUILD_MESSAGES',
-      Intents?.FLAGS?.GUILD_MEMBERS || 'GUILD_MEMBERS',
-    ];
-
-const partials = Partials
-  ? [
-      Partials.Channel,
-      Partials.Message,
-      Partials.User,
-      Partials.GuildMember,
-      Partials.Reaction,
-    ]
-  : [];
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({
-  intents,
-  partials,
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction,
+  ],
 });
 
 module.exports = client;
